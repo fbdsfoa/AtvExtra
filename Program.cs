f705fc19e700d1efa22dc46903ac8c14228cf697
FBrandaoSilva <filipebrandao.dasilva@gmail.com>
2023-09-26 21:05:45 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtvExtra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Filipe", "Rafael", "Rosen" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+
+                Console.WriteLine($"Hello {name.ToLower()}!");
+
+                Console.WriteLine($"Hello {name.Length}!");
+            }
+        }
+    }
+}
